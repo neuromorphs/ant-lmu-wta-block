b9ea82e385f5b34fda4c7e5ca58b01f8c2fa2724
Terry Stewart <tcstewar@uwaterloo.ca>
2021-08-07 17:14:41 -0400

    Fixed flake8 formatting errors

--- lmu_wta/tests/test_lowpass.py
+++ lmu_wta/tests/test_lowpass.py
@@ -1,7 +1,7 @@
 from lmu_wta import LMUWTABlock
-import matplotlib.pyplot as plt
 import numpy as np
 import nengo
+
 
 def test_basic_lowpass():
     lmu = LMUWTABlock(q=1, theta=0.1, n_neurons=1, size_in=1, size_out=1)
@@ -13,16 +13,8 @@ def test_basic_lowpass():
         y.append(lmu.step([1]))
 
     filt = nengo.synapses.Lowpass(0.1)
-    ideal = filt.filt(np.ones((1000,1)))
+    ideal = filt.filt(np.ones((1000, 1)))
 
     y = np.array(y)
-    print(y.shape, ideal.shape)
 
     assert np.allclose(y, ideal)
-
-    #plt.plot(ideal, lw=3, ls='--', label='ideal')
-    #plt.plot(y, label='output')
-    #plt.legend()
-    #plt.show()
-
-
