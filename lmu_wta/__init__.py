25d149e0afe7ce54f2ac4070c0b4314de187db67
Terry Stewart <tcstewar@uwaterloo.ca>
2021-08-07 20:47:09 -0400

    Fixed flake8 for __init__

--- lmu_wta/__init__.py
+++ lmu_wta/__init__.py
@@ -1,2 +1,5 @@
 from .lmu_wta import LMUWTABlock
 from .neuron_types import RectifiedLinear, Sigmoid, Tanh, LIFRate, Linear
+
+__all__ = ['LMUWTABlock', 'RectifiedLinear', 'Sigmoid', 'Tanh', 'LIFRate',
+           'Linear']
