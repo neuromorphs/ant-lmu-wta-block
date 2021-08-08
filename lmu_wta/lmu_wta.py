3588871fc6f6b5283c3ed991788c412e9298e915
Terry Stewart <tcstewar@uwaterloo.ca>
2021-08-07 20:45:00 -0400

    Added neuron_type parameter

--- lmu_wta/lmu_wta.py
+++ lmu_wta/lmu_wta.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.linalg
 
+from . import neuron_types
+
 
 def generate_LMU(theta, q, dt):
     A = np.zeros((q, q))
@@ -20,7 +22,8 @@ def generate_LMU(theta, q, dt):
 
 
 class LMUWTABlock(object):
-    def __init__(self, q, theta, n_neurons, size_in, size_out, dt=0.001):
+    def __init__(self, q, theta, n_neurons, size_in, size_out,
+                 neuron_type=neuron_types.RectifiedLinear(), dt=0.001):
         self.theta = theta
         self.q = q
         self.dt = dt
@@ -29,6 +32,8 @@ class LMUWTABlock(object):
         self.m = np.zeros((q, size_in))
         self.a = np.zeros(n_neurons)
 
+        self.neuron_type = neuron_type
+
         # TODO: add methods to generate E, D, and bias
         self.E = np.zeros((n_neurons, q * size_in))
         self.D = np.zeros((size_out, n_neurons))
@@ -36,15 +41,10 @@ class LMUWTABlock(object):
 
         # TODO: add WTA option
 
-    def nonlinearity(self, J):
-        # TODO: add other neuron models?
-        # rectified linear
-        return np.maximum(J, 0)
-
     def step(self, x):
         x = np.asarray(x)
         self.m[:] = self.Ad.dot(self.m) + self.Bd.dot(x[None, :])
         J = self.E.dot(self.m.flat) + self.bias
-        self.a[:] = self.nonlinearity(J)
+        self.a[:] = self.neuron_type.step(J)
         y = self.D.dot(self.a)
         return y
